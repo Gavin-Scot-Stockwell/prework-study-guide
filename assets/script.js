dc341f337556ea9f0e9e5d38dc59feb50f526eb1
Gavin <gsstockwell@gmail.com>
2024-09-26 13:45:46 -0700

    added java code into the assets

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+/*
+const topic = "HTLM";
+if (topic === 'HTML') {
+  console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+  console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+  console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+  console.log("Let's study JavaScript!");
+} else {
+  console.log('Please try again!');
+}
+*/
+
+const shapes = ["triangle", "square", "pentagon", "circle"];
+for(let x = 0; x < shapes.length; x++) {
+ console.log(shapes[x]);
+}
+
+const topics = ["HTML", "CSS", "Git", "JavaScript0"];
+for(let y = 0; y < topics.length; y++) {
+ console.log(topics[y])
+} 
